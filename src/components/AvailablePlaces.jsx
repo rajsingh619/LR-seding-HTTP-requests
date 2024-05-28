7dbede472da7d24da37c52f62e34a1ab4a41234a
rajsingh619 <rajsingh.mailto@gmail.com>
2024-05-28 16:12:37 +0530

    Initial commit:

---
+++ src/components/AvailablePlaces.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import Places from './Places.jsx';
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  
+
+  return (
+    <Places
+      title="Available Places"
+      places={[]}
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
